d5dbd366975d2afd8b459dd079cf773c6b285fc2
Stefan Wallin <stefan.w@festiz.com>
2018-04-21 08:15:56 +0200

    Separate spec configurations from stock config files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,8 +10,6 @@ require 'rspec/rails'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
-require 'ffaker'
-
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -61,6 +59,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   # Include spec/support files
-  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 end
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
---
+++ spec/support/ffaker.rb
@@ -0,0 +1,6 @@
+require 'ffaker'
+
+RSpec.configure do |config|
+  config.before(:all)  { FFaker::Random.seed=config.seed }
+  config.before(:each) { FFaker::Random.reset! }
+end
